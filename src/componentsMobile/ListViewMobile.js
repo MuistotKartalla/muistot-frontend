ea721ed52754757697c0b0b6f163207a38688276
Sistemshdortiz <90852224+Sistemshdortiz@users.noreply.github.com>
2023-04-11 15:07:39 +0300

    filter_by
    added filter

--- src/componentsMobile/ListViewMobile.js
+++ src/componentsMobile/ListViewMobile.js
@@ -26,22 +26,21 @@ export const ListViewMobile = (props) => {
     Mobile version of listview component, doesn't include postview, every list entry will
     navigate to relevant postview id via router.
   */
-  const [posts, setPosts] = useState(props.posts)
+  const [posts, setPosts] = useState({ data: props.posts, filter_by: "" });
   const myRef = useRef(props.listView)
   const itemsRef = useRef([]);
 
   useEffect(() => {
-    if(itemsRef !== [] && props.listView !== 0)
-    {
+    if (itemsRef !== [] && props.listView !== 0) {
       console.log(props.listView);
       itemsRef.current[props.listView].scrollIntoView()
     }
 
-  
+
   }, [])
 
   useEffect(() => {
-    if(posts.length === 0){
+    if (posts.length === 0) {
       setPosts(props.posts)
 
     }
@@ -53,10 +52,10 @@ export const ListViewMobile = (props) => {
     props.history.push(`/post-view/${post.id}`)
   }
 
-    /* const getDateFromUnixStamp = (unix) => {
-    const date = new Date(unix)
-    return `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`
-  }*/
+  /* const getDateFromUnixStamp = (unix) => {
+  const date = new Date(unix)
+  return `${date.getDate()}.${date.getMonth()+1}.${date.getFullYear()}`
+}*/
 
   const toMapView = (event) => {
     event.preventDefault()
@@ -67,33 +66,56 @@ export const ListViewMobile = (props) => {
     //New post onClick event handler.
     event.preventDefault()
     //console.log("Adding new post")
-    if(props.user !== null){
+    if (props.user !== null) {
       //console.log("Adding new post")
       props.history.push("/new-post/")
-    }else{
+    } else {
       //if not logged in, redirect to login page
       props.history.push("/login/")
       props.notify(props.settings.strings["login_required_to_post"], false, 5)
     }
   }
 
+  const handleFilterChange = (event) => {
+    const filterValue = event.target.value.toLowerCase();
+    const filteredPosts = props.posts.filter((post) => {
+      return post.title.toLowerCase().includes(filterValue);
+    });
+    setPosts({ data: filteredPosts, filter_by: filterValue });
+  };
+
+
 
   return (
 
     <div className="postListContainerMobile">
       <div className="postTitleContainerMobile">
-      <button className="mobileButtonContainer">
-        <ReturnIcon className="mobileIcon" onClick={toMapView}/>
-      </button>
-      <div className="postTitleHeader">
-      <h1 className="postTitleTextMobile">{props.settings.strings["list_view"]}</h1>
-      </div>
+        <button className="mobileButtonContainer">
+          <ReturnIcon className="mobileIcon" onClick={toMapView} />
+        </button>
+        <div className="postTitleHeader">
+          <h1 className="postTitleTextMobile">{props.settings.strings["list_view"]}</h1>
+          
+          <div className="postListFilterContainer">
+            <label htmlFor="postListFilterInput" className="postListFilterLabel">
+              Filter by title:
+            </label>
+            <input
+              type="text"
+              id="postListFilterInput"
+              className="postListFilterInput"
+              value={posts.filter_by}
+              onChange={handleFilterChange}
+            />
+          </div>
+
+        </div>
       </div>
       <div className="postListContainerInner">
         <ul className="postSearchList">
-          {posts.map((post,index) =>
-            <li key={index} tabIndex="1" ref={el => itemsRef.current[index] = el} className="postViewListItem" 
-            onClick={() => {onItemClick(post); itemsRef.current[index].scrollIntoView({behavior: 'smooth'}); props.updateListView(index)}}>
+          {posts.data.map((post, index) =>
+            <li key={index} tabIndex="1" ref={el => itemsRef.current[index] = el} className="postViewListItem"
+              onClick={() => { onItemClick(post); itemsRef.current[index].scrollIntoView({ behavior: 'smooth' }); props.updateListView(index) }}>
               <div className="postListItemImageContainer">
                 <img className="postListImagePreview" src={getImageURL(post.image)} alt=""></img>
               </div>
@@ -105,7 +127,7 @@ export const ListViewMobile = (props) => {
           )}
         </ul>
       </div>
-      {props.currentProject.id !== "parantolat"?
+      {/* {props.currentProject.id !== "parantolat"?
       <button className="mobileNewButton" onClick={newPostClick}>
         <AddIcon className="mobileIconSecondary"/>
       </button>
@@ -114,7 +136,7 @@ export const ListViewMobile = (props) => {
 
       <button className="mobileListViewButton" onClick={toMapView}>
         <MapViewIcon className="mobileIconSecondary"/>
-      </button>
+      </button> */}
     </div>
 
 
