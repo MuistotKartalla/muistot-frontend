13d133b384ce96f4735e27cb7b9ded0985177df0
Aaaitor <aitorpeto2002@gmail.com>
2023-06-16 09:39:42 +0300

    Fixes on user location

--- src/common components/UserLocationButton.js
+++ src/common components/UserLocationButton.js
@@ -1,25 +1,15 @@
-import React from 'react';
-
-export const Location = (props) => {
-  const locateUser = () => {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function (position) {
-        const lat = position.coords.latitude;
-        const lon = position.coords.longitude;
-        // Aquí puedes utilizar la información de latitud y longitud como necesites
-        console.log('Latitud:', lat);
-        console.log('Longitud:', lon);
-      }, function (error) {
-        alert('No se pudo obtener la ubicación: ' + error.message);
-      });
-    } else {
-      alert('Geolocalización no es soportada por este navegador.');
-    }
-  }
-
-  return (
-    <div className="locationContainer">
-      <button className="locationButton" onClick={locateUser}>Mi Ubicación</button>
-    </div>
-  );
+const userLocation = (props) =>{
+  return(
+    <div>
+    <button onClick={redirectToUserLocation}>Ir a mi ubicación</button>
+    <MapContainer center={props.initialPosition} zoom={13}>
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution="© OpenStreetMap contributors"
+      />
+    </MapContainer>
+  </div>
+  )
 }
+
+export default userLocation;
