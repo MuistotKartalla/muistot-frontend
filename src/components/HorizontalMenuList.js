bcad56dd2c026ebdacaecaa2497290c9a32b5715
Ville Nättinen <villenattinen@gmail.com>
2023-11-13 11:58:44 +0200

    Refactor code

--- src/components/HorizontalMenuList.js
+++ src/components/HorizontalMenuList.js
@@ -135,24 +135,24 @@ const HorizontalMenuList = (props) => {
             <div>
               {props.currentProject?.moderators?.find(user => user === props.user.username)?
                   <AcountDropDown items={[
-                    {string:props.settings.strings["my_projects"], onClickHandler: myProjectsClick},
-                    {string:props.settings.strings["my_posts"], onClickHandler: myPostsClick},
-                    {string:props.settings.strings["unverified-posts"], onClickHandler: UnverifiedPostsClick},
-                    {string:props.settings.strings["imageless_posts"], onClickHandler: ImagelessPostsClick},
-                    {string:props.settings.strings["about"], onClickHandler:aboutClick},
-                    {string:props.settings.strings["profile"], onClickHandler:ProfileClick},
-                    {string:props.settings.strings["project_management"], onClickHandler:ManagementClick},
-                    {string:props.settings.strings["log_out"], onClickHandler: logoutClick}
+                    { string: props.settings.strings["my_projects"], onClickHandler: myProjectsClick },
+                    { string: props.settings.strings["my_posts"], onClickHandler: myPostsClick },
+                    { string: props.settings.strings["unverified-posts"], onClickHandler: UnverifiedPostsClick },
+                    { string: props.settings.strings["imageless_posts"], onClickHandler: ImagelessPostsClick },
+                    { string: props.settings.strings["about"], onClickHandler: aboutClick },
+                    { string: props.settings.strings["profile"], onClickHandler: ProfileClick },
+                    { string: props.settings.strings["project_management"], onClickHandler: ManagementClick },
+                    { string: props.settings.strings["log_out"], onClickHandler: logoutClick }
                     ]}>
                     <p className="userNameText">{props.user.username}</p>
                   </AcountDropDown>
                   :
                   <AcountDropDown items={[
-                    {string:props.settings.strings["my_projects"], onClickHandler: myProjectsClick},
-                    {string:props.settings.strings["my_posts"], onClickHandler: myPostsClick},
-                    {string:props.settings.strings["about"], onClickHandler:aboutClick},
-                    {string:props.settings.strings["profile"], onClickHandler:ProfileClick},
-                    {string:props.settings.strings["log_out"], onClickHandler: logoutClick},
+                    { string: props.settings.strings["my_projects"], onClickHandler: myProjectsClick },
+                    { string: props.settings.strings["my_posts"], onClickHandler: myPostsClick},
+                    { string: props.settings.strings["about"], onClickHandler: aboutClick },
+                    { string: props.settings.strings["profile"], onClickHandler:ProfileClick },
+                    { string: props.settings.strings["log_out"], onClickHandler: logoutClick },
                   ]}>
                     <p className="userNameText">{props.user.username}</p>
                   </AcountDropDown>
